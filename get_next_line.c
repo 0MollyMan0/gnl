9b29c87427a13197fcecd9c6058cd44cd98932c5
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-04 11:13:41 +0100

    end morning

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 09:41:39 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/04 11:03:12 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/04 11:11:55 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,15 @@ char	*get_next_line(int fd)
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (read(fd, buffer, BUFFER_SIZE) > 0)
 	{
-		ft_strcat(stash, buffer);
+		stash = ft_strjoin(stash, buffer);
 		if ((i = strchr(stash)) != NULL)
 		{
 			x = 0;
 			while (x < i+1)
+			{
 				write(1, stash[i++], 1);
+				x++;
+			}
 		}
 	}
 	return (NULL);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,29 +6,11 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 09:42:02 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/04 11:06:34 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/04 11:10:32 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if ((unsigned char)s[i] == (unsigned char)c)
-		{
-			return ((char *)s + i);
-		}
-		i++;
-	}
-	if ((unsigned char)c == '\0')
-		return ((char *)s + ft_strlen(s));
-	return (NULL);
-}
 
 size_t	ft_strlen(const char *s)
 {
@@ -38,6 +20,22 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)s + ft_strlen(s));
+	return (NULL);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
