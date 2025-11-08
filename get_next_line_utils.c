aa14d9fceedc016d853d79b98189d4e2f8f4c588
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-08 09:22:26 +0100

    norm ok and add minijoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -40,6 +40,27 @@ int	ft_strchr_i(char *s, char c)
 	return (-1);
 }
 
+static char	*ft_minijoin(char *s1, char *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new)
+		return (NULL);
+	while (s1[j])
+		new[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		new[i++] = s2[j++];
+	new[i] = '\0';
+	free(s1);
+	return (new);
+}
+
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*new;
@@ -57,16 +78,7 @@ char	*ft_strjoin(char *s1, char *s2)
 			return (NULL);
 		s1[0] = '\0';
 	}
-	new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!new)
-		return (NULL);
-	while (s1[j])
-		new[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		new[i++] = s2[j++];
-	new[i] = '\0';
-	free(s1);
+	new = ft_minijoin(s1, s2);
 	return (new);
 }
 
